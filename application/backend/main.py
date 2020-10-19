f3d32c8ed9cbb1219bbd7e1f090d911d3a7c281a
JosephBabel <63685906+JosephBabel@users.noreply.github.com>
2020-10-19 14:07:53 -0700

    Jbabel/vertical prototype (#26)
    * vertical-prototype: Created skeleton files for backend and included some examples.
    
    * vertical-prototype: Added examples to main
    
    * veritcal-prototype: Fixed skeleton code not being saved.
    
    Co-authored-by: JosephBabel <jbabel@mail.sfsu.edu>

--- application/backend/main.py
+++ application/backend/main.py
@@ -1,8 +1,37 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+
 from starlette.responses import RedirectResponse
 from starlette.staticfiles import StaticFiles
 
+from . import crud, models
+from .database import SessionLocal, engine
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+@app.post("/items/", response_model=schemas.Item)
+def create_item(
+    item: schemas.ItemCreate, db: Session = Depends(get_db)
+):
+    return crud.create_item(db=db, item=item)
+
+
+@app.get("/items/", response_model=List[schemas.Item])
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    items = crud.get_items(db, skip=skip, limit=limit)
+    return items
 
 
 @app.get("/")
