65d05db1e7ec7479d444991d6d6d1d7df470c254
Lukas Pettersson <lucke97@gmail.com>
2020-10-21 13:36:02 -0700

    removed .idea files and fixed router

--- application/backend/routers.py
+++ application/backend/routers.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 
 from sqlalchemy.orm import Session
 
 from application.backend import crud
+from application.backend.main import get_db
 
 router = APIRouter()
 
@@ -10,6 +11,6 @@ router = APIRouter()
 # TODO - has not been tested yet
 
 # returns a JSON formatted response of listings from the %like search
-@router.get("/search/{db}{query_string}")
-async def read_listings(db: Session, query_string: str):
+@router.get("/search/{query_string}")
+async def read_listings(query_string: str, db: Session = Depends(get_db)):
     return {"listings", crud.get_posts_for_search(db, query_string)}
