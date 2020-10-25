fd3af11f96df82b5db8b463ccd1b71a2a22187e0
Niall Healy <48368862+niall-healy@users.noreply.github.com>
2020-10-24 18:19:50 -0700

    Nhealy/vertical prototype (#42)
    * Created get_posts_for_search method in crud.py w/%like functionality
    
    * added router.py to route /search/{db}{query_string}
    
    * removed .idea files and fixed router
    
    * fixed inconsistent naming: Post -> Listing, commented out unused code
    
    * added category check to get listings method, router
    
    * updated router function parameter names to match frontend
    
    * overhauled backend package/folder structure
    
    * fixed user in db url in database.py
    
    * set response model for router and make schema fields optional
    
    * Add backend search functionality for an Any category
    
    Co-authored-by: Lukas Pettersson <lucke97@gmail.com>
    Co-authored-by: Aaron Lander <36257200+aaronlander93@users.noreply.github.com>

--- application/backend/app/sql_db/crud.py
+++ application/backend/app/sql_db/crud.py
@@ -1,13 +1,16 @@
 from sqlalchemy.orm import Session
 
-
 from app.sql_db import models
 
 
 def get_listings_for_search(db: Session, searchQuery: str, category: str):
     if len(searchQuery) == 0:
-        return db.query(models.Listing).all()
-    else:
+        retVal = db.query(models.Listing).all()
+    elif category != 'Any':
         # Note: use like() for case sensitivity, ilike() for case insensitivity
-        return db.query(models.Listing).filter(models.Listing.name.ilike('%' + searchQuery + '%'),
-                                               models.Listing.category == category).all()
+        retVal = db.query(models.Listing).filter(models.Listing.name.ilike('%' + searchQuery + '%'),
+                                                 models.Listing.category == category).all()
+    else:
+        retVal = db.query(models.Listing).filter(models.Listing.name.ilike('%' + searchQuery + '%')).all()
+
+    return retVal
