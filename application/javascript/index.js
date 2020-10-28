de29e378c0f0fd39724ee32bcd62d990f96925cd
Aaron Lander <36257200+aaronlander93@users.noreply.github.com>
2020-10-28 12:33:08 -0700

    Change searches to all be done through form submissions. Add search persistence.

--- application/javascript/index.js
+++ application/javascript/index.js
@@ -1,43 +1,19 @@
 window.onload = function() {
-   function search(){
-      let text = document.getElementById("search-bar").value;
-      text = text.replace(" ", "%20");
+   var search = new URLSearchParams(window.location.search);
+   var category = search.get('category');
+   var keywords = search.get('keywords');
 
-      let category = document.getElementById("category").value;
-
-      var fetchOptions = {
-		method: "GET",
-      };
-
-      let fetchURL = '/search/?category=' + category  + "&keywords=" + text;
-      fetch(fetchURL, fetchOptions)
-      .then((data) => {
-        return data.json() 
-      })
-      .then((jsonData) => {
-         localStorage.setItem('results', JSON.stringify(jsonData));
-         window.location.href= "http://ec2-3-21-104-38.us-east-2.compute.amazonaws.com/html/results.html";
-      })
-      .catch((err) => {
-       	 console.log(err);
-      })
+   if(category){
+      document.getElementById('category').value = category;
+   }
+   else {
+      document.getElementById('category').value = 'Any';
    }
 
-   document.getElementById("search-button").addEventListener('click', search);
-   var resultField = document.getElementById("results");
-   
-   if(resultField) {
-      var results = JSON.parse(localStorage.getItem('results')); 
-      var html = "";
-   
-      results.forEach((result) => {
-         html += "<img src='" + result['photo'] + "'/>";
-         html += "<p><b>Item Name: </b> " + result['name'] + "</p>\n";
-         html += "<p><b>Description: </b> " + result['description'] + "</p>\n";
-         html += "<p><b>Price: </b> " + result['price'] + "</p>\n";
-         html += "<br>\n";
-      });
-
-      resultField.innerHTML = html;
+   if(keywords){
+      document.getElementById('search-bar').value = keywords;
+   }
+   else {
+      document.getElementById('search-bar').placeholder = "Search..."
    }
 }
