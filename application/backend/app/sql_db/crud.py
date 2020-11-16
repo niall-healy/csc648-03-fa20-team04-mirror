cadb217cf0a08cd00e590966d19783475742b4d2
Niall Healy <48368862+niall-healy@users.noreply.github.com>
2020-11-16 15:34:18 -0800

    search checks description too (#101)

--- application/backend/app/sql_db/crud.py
+++ application/backend/app/sql_db/crud.py
@@ -1,6 +1,6 @@
 import datetime
 
-from sqlalchemy import desc
+from sqlalchemy import desc, or_
 from sqlalchemy.orm import Session
 
 from app.sql_db import models, schemas
@@ -34,10 +34,12 @@ def get_listings_for_search(db: Session, searchQuery: str, category: str):
             retVal = db.query(models.Listing).filter(models.Listing.category == category).all()
     elif category != 'Any':
         # Note: use like() for case sensitivity, ilike() for case insensitivity
-        retVal = db.query(models.Listing).filter(models.Listing.name.ilike('%' + searchQuery + '%'),
-                                                 models.Listing.category == category).all()
+        retVal = db.query(models.Listing).filter(models.Listing.category == category,
+                                                 or_(models.Listing.name.ilike('%' + searchQuery + '%'),
+                                                     models.Listing.description.ilike('%' + searchQuery + '%'))).all()
     else:
-        retVal = db.query(models.Listing).filter(models.Listing.name.ilike('%' + searchQuery + '%')).all()
+        retVal = db.query(models.Listing).filter(or_(models.Listing.name.ilike('%' + searchQuery + '%'),
+                                                     models.Listing.description.ilike('%' + searchQuery + '%'))).all()
 
     return retVal
 
