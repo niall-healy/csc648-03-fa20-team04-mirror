655bbcd0e451d134a44b87bba9ba4fb79912b5f2
JosephBabel <63685906+JosephBabel@users.noreply.github.com>
2020-11-16 22:33:35 -0800

    M3/results page (#107)
    * style results page and render on front end
    
    * condense list and alternate colors
    
    * change backend router to HTMLResponse
    
    * Use prod server database URL
    
    * add link to listing page
    
    * Fixed contact seller button not activating
    
    * add better organization to stylesheet
    
    * add price filter

--- application/javascript/results.js
+++ application/javascript/results.js
@@ -1,13 +1,13 @@
 // Load the search results from the server into the html
 function loadListings(dataJson) {
-    var _html = '';
-    var listOdd = true;
+    let _html = '';
+    let isOdd = true;
     for (listing in dataJson) {
         _html +=
             '<li onclick="location.href=\'/listing/?id=' +
             dataJson[listing].id +
             '\'" class="list-group-item list-group-item-action p-2 ' +
-            (listOdd ? 'list-group-item-1' : 'list-group-item-2') +
+            (isOdd ? 'list-group-item-1' : 'list-group-item-2') +
             '">' +
             '<div class="row no-gutters">';
         _html +=
@@ -25,26 +25,70 @@ function loadListings(dataJson) {
             dataJson[listing].description +
             '</p></div></div>';
         _html +=
-            '<div class="col p-2"><p><b>Price: </b>$' +
+            '<div class="col d-flex p-2"><b>Price: </b>$' +
+            '<div class="price">' +
             dataJson[listing].price +
-            '</div>';
+            '</div></div>';
         _html +=
             '<div class="col-sm-12 col-md p-2">' +
             '<button type="button" class="btn btn-primary btn-block pull-right no-overflow" onclick="event.stopPropagation()">Contact Seller</button>';
         _html += '</div></div></li>';
 
-        if (listOdd) {
-            listOdd = false;
-        } else {
-            listOdd = true;
-        }
+        isOdd = !isOdd;
     }
 
-    document.getElementById('results').innerHTML = _html;
+    $('#results').html(_html);
+    displayResults();
+}
+
+// Apply alternating colors
+function reapplyAlternatingColors() {
+    let isOdd = true;
+    $('#main-body li').each(function () {
+        if ($(this).css('display') != 'none') {
+            $(this).removeClass();
+            $(this).attr('class', 'list-group-item list-group-item-action p-2');
+            (isOdd ? $(this).addClass('list-group-item-1') : $(this).addClass('list-group-item-2'));
+            isOdd = !isOdd;
+        }
+    });
+}
+
+// Applys price filter
+function filterPrice() {
+    let min = $('#price-min-field').prop('value');
+    let max = $('#price-max-field').prop('value');
+    if (max == '' || max == 2000) {
+        max = Number.MAX_SAFE_INTEGER;
+        $('#price-max-field').prop('value', '');
+    }
+
+    $('#main-body li').each(function () {
+        let price = $(this).find('.price');
+        if (parseInt(price.html()) < min || parseInt(price.html()) > max) {
+            $(this).css('display', 'none');
+        } else {
+            $(this).css('display', 'grid');
+        }
+    });
+
+    displayResults();
+    reapplyAlternatingColors();
+}
+
+// Displays number of results
+function displayResults() {
+    let counter = 0;
+    $('#main-body li').each(function () {
+        if ($(this).css('display') != 'none') {
+            counter++;
+        }
+    });
+    $('.num-results').html('<h5>Showing ' + counter + ' results...</h5>');
 }
 
 // Send get request with the search parameters
-$(document).ready(function() {
+$(document).ready(function () {
     var search = new URLSearchParams(window.location.search);
     var category = search.get('category');
     var keywords = search.get('keywords');
@@ -78,4 +122,37 @@ $(document).ready(function() {
     } else {
         document.getElementById('search-bar').placeholder = 'Search...';
     }
+
+    // sync price sliders
+    $('#price-min').on('input', function () {
+        $('#price-min-field').prop('value', $(this).prop('value') * 10);
+    });
+
+    $('#price-max').on('input', function () {
+        $('#price-max-field').prop('value', $(this).prop('value') * 10);
+    });
+
+    $('#price-min-field').on('input', function () {
+        $('#price-min').prop('value', $(this).prop('value') / 10);
+    });
+
+    $('#price-max-field').on('input', function () {
+        $('#price-max').prop('value', $(this).prop('value') / 10);
+    });
+
+    // apply filters
+    $('#apply-button').on('click', function () {
+        filterPrice();
+        $(this).prop('aria-pressed', 'false');
+        $(this).blur();
+    });
+
+    // flip filter arrow on collapse
+    $('#collapsable').on('show.bs.collapse', function () {
+        $('.rotate').toggleClass('fa-rotate-180');
+    });
+
+    $('#collapsable').on('hide.bs.collapse', function () {
+        $('.rotate').toggleClass('fa-rotate-180');
+    });
 });
