2ca773d9ddd693af5924f9b22bd69bb82aaf23db
LukasPettersson <60670371+LukasPettersson@users.noreply.github.com>
2020-11-16 17:32:14 -0800

    message post functionality for backend (#105)
    create message post router, make create_message function in crud, delete model for message thread, reconfigured model for message, delete message thread schema, reconfigure message schema, include router in main
    
    Co-authored-by: Lukas Pettersson <lucke97@gmail.com>

---
+++ application/backend/app/routers/message.py
@@ -0,0 +1,22 @@
+from fastapi import APIRouter, Depends, Form
+from sqlalchemy.orm import Session
+
+from application.backend.app.routers.login import get_current_user
+from application.backend.app.sql_db import schemas, crud
+from application.backend.app.sql_db.database import get_db
+
+router = APIRouter()
+
+
+@router.post("/", response_model=schemas.Message)
+async def create_message(db: Session = Depends(get_db),
+                         message: str = Form(...),
+                         listing_id: int = Form(...)):
+    return crud.create_message(db, message, listing_id)
+
+
+"""
+@router.get("/", response_model=schemas.Message)
+async def get_message_by_seller_id(db: Session = Depends(get_db()),
+                                    id: int):
+"""
