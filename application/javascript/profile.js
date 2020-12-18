af3669963c22fe6b3e37b43c9c09302a61614986
JosephBabel <theodorebabel@gmail.com>
2020-12-17 16:45:50 -0800

    initial display listings

--- application/javascript/profile.js
+++ application/javascript/profile.js
@@ -6,6 +6,7 @@ Author: Joseph Babel
 */
 
 let allMessages;
+let allListings;
 
 function loadMessages() {
     let _html = '';
@@ -44,6 +45,49 @@ function loadMessages() {
     $('#messages').html(_html);
 }
 
+// Load the search results from the server into the html
+function loadListings() {
+    let _html = '';
+
+    for (listing in allListings) {
+    let isOdd = true;
+    let isTextbook = allListings[listing].course != null;
+
+        _html +=
+            '<li id="' +
+            allListings[listing].id +
+            '" class="list-group-item list-group-item-action p-2 ' +
+            (isOdd ? 'list-group-item-1' : 'list-group-item-2') +
+            '">' +
+            '<div class="row no-gutters">';
+        _html +=
+            '<div class="col-lg-10 d-flex">' +
+            '<img class="thumbnail rounded" src="' +
+            allListings[listing].photoPaths[0].thumbnailPath +
+            '">';
+        _html +=
+            '<div class="p-2">' +
+            '<h5><b>' +
+            allListings[listing].name +
+            '</b></h5>';
+        _html +=
+            '<p class="text-secondary no-overflow">' +
+            allListings[listing].description +
+            '</p></div></div>';
+        _html +=
+            '<div class="col p-2"><b>Price: </b>$' +
+            allListings[listing].price +
+            '<br><br>' +
+            (isTextbook ? '<b>Course: </b>' +
+            allListings[listing].course : '') +
+            '</div></div></li>'
+
+        isOdd = !isOdd;
+    }
+
+    $('#results').html(_html);
+}
+
 $(document).ready(function () {
     let userJSON = localStorage.getItem('loggedInUser');
     let fetchOptions;
@@ -69,4 +113,15 @@ $(document).ready(function () {
             allMessages = dataJson;
             loadMessages()
         })
+
+    fetchURL = '/listing/user/'
+
+    fetch(fetchURL, fetchOptions)
+    .then((response) => {
+        return response.json();
+    })
+    .then((dataJson) => {
+        allListings = dataJson;
+        loadListings();
+    })
 });
